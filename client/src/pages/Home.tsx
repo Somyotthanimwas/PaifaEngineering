192b205a170a000a3cd0eeeadcb3754df8748cef
Somyot Thanimwas <plaifaeng@gmail.com>
2026-09-13 14:22:55 +0700

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -177,7 +177,7 @@ export default function Home() {
             className="hidden items-center gap-8 lg:flex"
             aria-label="Main navigation"
           >
-            <a className="nav-link !text-[28px] text-green-700 hover:text-green-500" href="#about">
+            <a className="nav-link !text-[26px] text-green-700 hover:text-green-500" href="#about">
               เกี่ยวกับเรา
             </a>
             <a className="nav-link text-2xl text-green-700 hover:text-green-500" href="#services">
